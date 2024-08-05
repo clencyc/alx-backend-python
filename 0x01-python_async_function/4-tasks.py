7e16c3732e4968bb890bfbd86b71c675f8410b9e
clencyc <clency2023@gmail.com>
2024-08-05 17:26:09 +0300

    Tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,5 +6,14 @@ identical to wait_n except task_wait_random
 is being called.
 """
 
+import asyncio
+import random
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
 
-wait_n = __import__('3-tasks.py').wait_n
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Asynchronous coroutine that takes in 2 int arguments"""
+    list_of_delays: List[float] = [await task_wait_random
+                                   (max_delay) for i in range(n)]
+    return sorted(list_of_delays)
