6aa0750e11efb605146a9f74235a43a0daff936d
clencyc <clency2023@gmail.com>
2024-08-07 13:29:44 +0300

    Annotated the function

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -13,7 +13,7 @@ import random
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     measure_runtime coroutine that will execute async_comprehension
     four times in parallel using asyncio.gather.
